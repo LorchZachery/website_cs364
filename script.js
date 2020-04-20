6bc2a65295b07576acf1f8050a7546fd14883235
LorchZachery <lorchzachery@gmail.com>
2020-04-20 13:50:40 -0700

    more stuff

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
 function showExtra(value) {
 	var boulderDiv = document.getElementById('boulderDiv');
-	if(value == ""){
+	var sportDiv = document.getElementById('sportDiv');
+	var speedDiv = document.getElementById('speedDiv');
+	var length = document.getElementById('length');
+	var time = document.getElementById('speedTime');
+	var attempts = document.getElementById('attempts');
+	if(value == 0){
 		boulderDiv.style.display = "none";
+		sportDiv.style.display = "none";
+		speedDiv.style.display = "none";
 	}
 	else if(value == "boulder") {
 		boulderDiv.style.display = "block";
-		//$('#grade').attr('required','');
-		//$('#grade').attr('data-error', 'This Field is required');
-		//$('#typeOfGrade').attr('required','');
-		//$('#typeOfGrade').attr('data-error','This Field is required');
-	} else{
+		sportDiv.style.display = "none";
+		speedDiv.style.display = "none";
+		length.required = false;
+		time.required = false;
+		attempts.required = false;
+	} 
+	else if(value == "sport"){
+		sportDiv.style.display = "block";
 		boulderDiv.style.display = "none";
-		//$('#grade').removeAttr('required');
-		//$('#grade').removeAttr('data-error');
-		//$('#typeOfGrade').removeAttr('required');
-		//$('#typeOfGrade').removeAttr('data-error');
+		speedDiv.style.display = "none";
+		length.required = true;
+		time.required = false;
+		attempts.required = false;
+	}
+	else if(value =="speed"){
+		speedDiv.style.display="block";
+		boulderDiv.style.display = "none";
+		sportDiv.style.display="none";
+		length.required = false;
+		time.required = true;
+		attempts.required = true;
+	}
+	else{
+		boulderDiv.style.display = "none";
+		sportDiv.style.display = "none";
+		speedDiv.style.display="none";
+		length.required = false;
+		time.required = false;
+		attempts.required = false;
 	}
 }
+function timeCheck() {
 
+	var time = document.getElementById('speedTime').value; 
+	var speed = document.getElementById('duration').value;
+	if(time < 00 || isNaN(time) ||  time > 500){
+		alert("Time needs to be number with seconds and Miliseconds");
+	}
+	if(speed < 0 || isNaN(speed) || speed > 300){
+		alert("Time needs to be in Minutes and Greater than 0");
+	}
+}
+function checkValue() {
+	var value = document.getElementById('typeWorkout');
+	console.log(value);
+	
+	if(value == "none"){
+		alert("Must select disipline");
+	}
+}
 

cad4e52a7488dded728db47f0e264684524e484d
LorchZachery <lorchzachery@gmail.com>
2020-04-28 17:00:10 -0700

    more css stuff

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ include('authenticate.php')
 	<head>
 		<title>Rock Climbing Workouts</title>
 		<link href="format.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<style>
+		.slides {display:none;}
+		</style>
+		
 	</head>
 	<body>
 		<ul>
@@ -21,17 +26,25 @@ include('authenticate.php')
 			<li style="float:right"><a href="log_out.php">Log Out</a></li>	
 		</ul>
 		<h1>Rock Climbing Workouts 101</h1>
+		<div style="max-width:50%;" class="center_slide" >	
+		<img class="slides w3-animate-fading" src="images/img1.png" style="width:100%">
+		<img class="slides w3-animate-fading" src="images/img2.png" style="width:100%">
+		<img class="slides w3-animate-fading" src="images/img3.png" style="width:100%">
+		<img class="slides w3-animate-fading" src="images/img4.png" style="width:100%">
+		</div>
+		<script src="slideshow.js"></script>
 		<p style="text-align: center">This website is made so you can log your climbing workouts and see how you progress overtime.
 <br> The goal is that this will motivate you to keep training and work as hard as you can to become the best climber you can be</p>
 	<div class="center_div">	
-	<div class="zoom" style="background-image:url('images/workout.jpg');" ><a href="submit_data.php"><h3 style="color: black">Submit Workout</h3></a>
+	<div class="zoom" style="background-image:url('images/dumbbell-solid.svg');" ><a href="submit_data.php"></a>
 	</div>
-	<div class="zoom" style="background-image:url('images/met.png');"><a href="metrics.php"><h3 style="color: black">View Metrics</h3></a>
+	<div class="zoom" style="background-image:url('images/chart.svg');"><a href="metrics.php"></a>
 	</div>
-	<div class="zoom" style="background-image:url('images/report.png');"><a href="report.php"><h3 style="color: navy">View Progress Report</h3></a>
+	<div class="zoom" style="background-image:url('images/folder.svg');"><a href="report.php"></a>
         </div>
- 	<div class="zoom" style="background-image:url('images/look.png');"><a href="look_up.php"><h3 style="color: red">Look up Workouts</h3></a>
+ 	<div class="zoom" style="background-image:url('images/search-solid.svg');"><a href="look_up.php"></a>
 	</div>	
 	</div>
+
 </body>
 </html>
---
+++ slideshow.js
@@ -0,0 +1,13 @@
+var slideIndex =0;
+slides();
+function slides() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(slides, 9000); // Change image every 2 seconds
+}
